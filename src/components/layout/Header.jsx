cf44c4e45f357cd0e303937cce933a0687a38231
alateer <1265696937@qq.com>
2023-08-23 20:40:18 +0800

    update header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,22 @@
 function Header() {
-      return (
-            <>
-                  <h1>Header</h1>
-            </>
-      )
+	return (
+		<header className="header" data-testid="header">
+			<nav>
+				<div className="logo">
+					<img src="xxx" alt="Todoist" />
+				</div>
+
+				<div className="settings">
+					<ul>
+						<li className="settings__add">settings__add</li>
+						<li className="settings__darkmode">settings__add</li>
+					</ul>
+				</div>
+			</nav>
+
+
+		</header>
+	)
 }
 
 export default Header
